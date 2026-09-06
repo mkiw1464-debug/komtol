9fc562536cc2268d98d1dde622df940156551279
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-06 17:00:52 +0800

    Delete src/ui/Menu.h

--- src/ui/Menu.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#import <UIKit/UIKit.h>
-
-void InitMenu();
-void ToggleMenu();
-void HandleTap();
-
-// Exposed draw view so main.mm can call setNeedsDisplay
-extern UIView *drawView;
