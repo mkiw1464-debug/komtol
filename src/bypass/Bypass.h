4682c6f1a00d626945dcc63912f8f86b330ec1f3
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-06 17:00:33 +0800

    Add files via upload

---
+++ src/bypass/Bypass.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void InitAllBypasses();
+
+// Individual (boleh panggil manual)
+void BypassAntiBan();
+void BypassAntiCheat();
+void BypassLogin();
+void BypassLobby();
+void BypassReport();
+void BypassBlacklist();
+void BypassThirdPartyInstall();
+void PatchIntegrityCheck();
+void SpoofDeviceFingerprint();
+void HideLibraryFromTaskList();
+void PatchJailbreakDetection();
+void BypassMatchmaking();
